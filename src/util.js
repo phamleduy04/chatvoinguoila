59d0913f7fb45a53dff30f3572156a66ccbf0693
Duy Pham Le <phamleduy04@gmail.com>
2020-08-18 16:40:05 -0500

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,17 +1,17 @@
 /* eslint-disable prettier/prettier */
 const logPath = './log/log.txt'
+const { writeFileSync, readFileSync } = require('fs');
 module.exports = {
   logging: function (text) {
     if (!text) return;
     const moment = require('moment-timezone');
-    const { writeFileSync } = require('fs');
     const timenow = moment().tz(process.env.TIMEZONE || 'America/Chicago').format('lll');
-    writeFileSync(logPath, `\n${timenow} || ${text}`);
+    const oldData = readFileSync(logPath, 'utf-8');
+    writeFileSync(logPath, `${oldData}\n${timenow} || ${text}`);
   },
   exportLog: async function(){
     const { VultrexHaste } = require('vultrex.haste');
     const haste = new VultrexHaste({ url: "https://hasteb.in" });
-    const { readFileSync } = require('fs');
     const data = readFileSync(logPath, 'utf-8');
     try {
       let url = await haste.post(data)
