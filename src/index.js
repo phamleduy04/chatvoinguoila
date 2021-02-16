069abc624738dde6c7eaed468366a9503c2f9c37
Duy Pham Le <phamleduy04@gmail.com>
2021-02-16 16:34:24 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -106,7 +106,7 @@ async function HandlePostBack(ctx) {
     case 'GET_STARTED': {
       let userprofile = await ctx.getUserProfile();
       await ctx.sendText(
-        `Chào mừng bạn ${userprofile.name} đã đến với Bất Tử bot!\nKhi bạn bấm nút "Tìm kiếm" có nghĩa là bạn đã đồng ý các điều khoản được ghi ở https://bit.ly/3iV6w81`
+        `Chào mừng bạn ${userprofile.name} đã đến với Bất Tử bot!\nKhi bạn bấm nút "Tìm kiếm" có nghĩa là bạn đã đồng ý các điều khoản được ghi ở https://bit.ly/3iV6w81\n\nLưu ý:Nếu bạn ở EU sẽ không sử dụng các nút được, bạn vui lòng nhắn "search" nhé!`
       );
       menu(ctx);
     }
