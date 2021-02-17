d5ecea511bf254f0e56e6e16b5a977a37e5453e2
Duy Pham Le <phamleduy04@gmail.com>
2021-02-17 13:51:00 -0600

    quá tải api

--- src/index.js
+++ src/index.js
@@ -72,6 +72,7 @@ async function HandleFile(ctx) {
 }
 
 async function HandleMessage(ctx) {
+  await sleep(500);
   let userid = ctx.event.rawEvent.sender.id;
   stats.messages++;
   let data = await getAsync(userid);
