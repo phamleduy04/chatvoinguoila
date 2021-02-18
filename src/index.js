197414afbc4c6f6ae85367c9e68022d9921e384d
Duy Pham Le <phamleduy04@gmail.com>
2021-02-17 18:19:16 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -102,7 +102,7 @@ async function HandleMessage(ctx) {
     cooldown.delete(userid);
   }, ms('10s'));
   // sleep để giảm tải cho bot
-  await sleep(8000);
+  await sleep(10000);
   if (!data) await standby(userid);
   let msgText = ctx.event.message.text.toLowerCase();
   // những lệnh chỉ có owner xài được
