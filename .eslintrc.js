cc55e6c7239ffa2d0360ba60de752cc075e4fc92
Duy Pham Le <phamleduy04@gmail.com>
2021-02-22 09:28:00 -0600

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,43 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2018,
+  "env": {
+      "es2020": true,
+      "node": true,
   },
-  extends: ['eslint:recommended', 'prettier'],
-  env: {
-    node: true,
+  "parserOptions": {
+      "ecmaVersion": 11,
+      "sourceType": "module",
   },
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'es5',
-        singleQuote: true,
-        'no-undef': false,
-      },
-    ],
+  "rules": {
+  "comma-dangle": ["error", "always-multiline"],
+  "comma-spacing": "error",
+  "comma-style": "error",
+  "dot-location": ["error", "property"],
+  "handle-callback-err": "off",
+  "max-nested-callbacks": ["error", { "max": 4 }],
+  "max-statements-per-line": ["error", { "max": 2 }],
+  "no-console": "off",
+  "no-empty-function": "error",
+  "no-floating-decimal": "error",
+  "no-inline-comments": "error",
+  "no-lonely-if": "error",
+  "no-multi-spaces": "error",
+  "no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 1, "maxBOF": 0 }],
+  "no-shadow": ["error", { "allow": ["err", "resolve", "reject"] }],
+  "no-trailing-spaces": ["error"],
+  "no-var": "error",
+  "object-curly-spacing": ["error", "always"],
+  "prefer-const": "error",
+  "space-before-blocks": "error",
+  "space-before-function-paren": ["error", {
+    "anonymous": "never",
+    "named": "never",
+    "asyncArrow": "always",
+  }],
+  "space-in-parens": "error",
+  "space-infix-ops": "error",
+  "space-unary-ops": "error",
+  "spaced-comment": "error",
+  "yoda": "error",
+  "semi": "error",
   },
-  plugins: ['prettier'],
-  overrides: [
-    {
-      files: ['**/*.test.js'],
-      env: {
-        jest: true,
-      },
-    },
-  ],
 };
